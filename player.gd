1efac48723945aee10b7c7e80f4d0317fe67ef7f
raptor0999 <raptordev@proton.me>
2024-12-18 01:26:25 -0500

    Initial project commit

---
+++ player.gd
@@ -0,0 +1,78 @@
+extends CharacterBody3D
+
+const LERP_VALUE : float = 0.15
+
+var walk_speed = 5.0
+var run_speed = 8.0
+var speed = walk_speed
+var JUMP_VELOCITY = 4.5
+
+var state
+@onready var playerMesh:Node3D = $characterSmall
+@onready var animationPlayer:AnimationPlayer = $AnimationPlayer
+@onready var horPivot:Node3D = $CamRoot/HorizontalPivot
+
+var mouseInput
+
+func _ready() -> void:
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion:
+		horPivot.rotate_y(-event.relative.x * 0.005)
+
+func _input(event: InputEvent) -> void:
+	if Input.is_action_pressed("run"):
+		state = "run"
+	else:
+		state = "walk"
+		
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		state = "jump"
+		velocity.y = JUMP_VELOCITY
+		
+	if Input.is_action_just_pressed("quit"):
+		get_tree().quit()
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+		animationPlayer.play("ImportedLib/jump")
+
+	# Handle jump.
+	if state == "jump":
+		animationPlayer.play("ImportedLib/jump")
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var input_dir = Vector3.ZERO
+	input_dir.x = Input.get_action_strength("strafe_right") - Input.get_action_strength("strafe_left")
+	input_dir.z = Input.get_action_strength("backward") - Input.get_action_strength("forward")
+	input_dir = input_dir.rotated(Vector3.UP, horPivot.rotation.y)
+	
+	#var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if input_dir:
+		velocity.x = input_dir.x * speed
+		velocity.z = input_dir.z * speed
+	else:
+		velocity.x = move_toward(velocity.x, 0, speed)
+		velocity.z = move_toward(velocity.z, 0, speed)
+		
+	if input_dir:
+		playerMesh.rotation.y = lerp_angle(playerMesh.rotation.y, atan2(velocity.x, velocity.z), LERP_VALUE)
+		
+	if is_on_floor():
+		if state == "run":
+			animationPlayer.play("ImportedLib/run")
+			speed = run_speed
+			
+		if state == "walk":
+			animationPlayer.play("ImportedLib/walk")
+			speed = walk_speed
+			
+		if velocity.length() < 0.1:
+			state = "idle"
+			animationPlayer.play("ImportedLib/idle")
+
+	move_and_slide()
